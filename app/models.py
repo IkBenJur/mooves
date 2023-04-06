a75c020dbd57ee03dcfcf16e5b7db3484327fe37
IkBenJur <jurspammer@gmail.com>
2023-04-06 18:14:59 +0200

    First commit

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f"{self.username}"
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+    
+class Genre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.String(64), index=True, unique=True)
+    movies = db.Relationship("Movie", backref="genre", lazy="dynamic")
+
+    def __repr__(self):
+        return f"{self.genre}"
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), index=True)
+    #db.ForeignKey gebruikt de SQLAlchemy table name dus lowercase (snakecase voor multi word)
+    #db.Relationship gebruikt de modelclass naam
+    genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"))
+
+    def __repr__(self):
+        return f"{self.title}"
