4a4c92a693481a6e338646e7fcfaf3f10ae5376f
IkBenJur <jurspammer@gmail.com>
2023-05-14 23:04:53 +0200

    Added genre model to flask shell context

--- mooves.py
+++ mooves.py
@@ -1,6 +1,6 @@
 from app import app, db
-from app.models import User, Review, Movie
+from app.models import User, Review, Movie, Genre
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Review": Review, "Movie": Movie}
+    return {"db": db, "User": User, "Review": Review, "Movie": Movie, "Genre": Genre}
